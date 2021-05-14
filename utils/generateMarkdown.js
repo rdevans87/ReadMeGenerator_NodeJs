1fca189a1aceea49eca2b3cc0bb903581e66e9f7
Ryan Evans <rdevans87@gmail.com>
2021-05-14 02:03:53 -0400

    generateMarkdown.js
    working on renderlicense badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,11 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
+ if (license === 'none') {
 
+
+
+ }
 
 
 }
@@ -9,16 +13,22 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
+if (license === 'none') {
 
 
+
+}
 
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
+if (license === 'none') {
 
 
+
+}
   
 }
 
